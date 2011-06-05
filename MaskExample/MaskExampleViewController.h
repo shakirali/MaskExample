7dde6b9eed71de800580d324030e125597368584
Shakir Ali <shakirmithani@gmail.com>
2011-06-05 11:41:13 +0100

    Initial Commit

---
+++ MaskExample/MaskExampleViewController.h
@@ -0,0 +1,15 @@
+//
+//  MaskExampleViewController.h
+//  MaskExample
+//
+//  Created by Shakir Ali on 05/06/2011.
+//  Copyright 2011 University of Edinburgh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MaskExampleViewController : UIViewController {
+    
+}
+
+@end
